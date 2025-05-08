495cfe7fb7f18f60d189852bb1a992bff16fa998
heartball1974 <heartball1974@gmail.com>
2025-05-08 14:16:42 +0900

    version

--- simpleBatch/src/main/java/com/example/simplebatch/SimpleBatchApplication.java
+++ simpleBatch/src/main/java/com/example/simplebatch/SimpleBatchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SimpleBatchApplication {
-
+/**/
     public static void main(String[] args) {
         SpringApplication.run(SimpleBatchApplication.class, args);
     }
